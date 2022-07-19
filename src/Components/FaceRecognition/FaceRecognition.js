c26ef72c7db3ecf1c8ef3509566f0ff2a2d15f0a
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-07-19 15:25:32 -0700

    boxes now perfectly surround face

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -3,9 +3,13 @@ import classes from "./FaceRecognition.module.css";
 
 export default function FaceRecognition({imageUrl, box}){
     return(
-        <div>
-            <img id='inputimage' className={classes.image} src={imageUrl} />
-            <div className={classes.bounding_box} style={{top: box.topRow, right:box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
+        <div className={classes.container}>
+            <div className={classes.inner_container}>
+                <img id='inputimage' className={classes.image} src={imageUrl} />
+                <div 
+                className={classes.bounding_box} style={{top: box.topRow, right:box.rightCol, bottom: box.bottomRow, left: box.leftCol }}>
+                </div>
+            </div>
         </div>
     );
 }
