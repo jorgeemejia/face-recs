ba4a7984f48c10ccc8ace433450cc2ae212007f3
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-08-10 17:20:41 -0700

    added serve

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -25,7 +25,7 @@ class Signin extends React.Component{
 
     onSubmitSignIn = () => {
         console.log(this.state);
-        fetch('http://localhost:3000/signin', {
+        fetch('https://warm-lake-52832.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
