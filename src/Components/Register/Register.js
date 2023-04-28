8718a0f53cd158d7c2b11d3abf369edbcbeacbfe
Jorge <jorgeemejia@csu.fullerton.edu>
2023-04-27 22:15:37 -0700

    deploy

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -27,7 +27,7 @@ class Register extends React.Component {
 
     onSubmitSignIn = () => {
         console.log(this.state);
-        fetch('http://localhost:3001/register', {
+        fetch('https://thawing-chamber-69828.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
